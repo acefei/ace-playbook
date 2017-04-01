28e473e44502751d655550ae69d25d3a590b5ea8
vagrant <vagrant@cdc-cafe-ci.calix.local>
2017-04-01 08:11:38 +0000

    Update roles/common

--- create_ansible_playbook_template.sh
+++ create_ansible_playbook_template.sh
@@ -1,31 +1,51 @@
 #!/bin/bash
 
-read -p "Set playbook name:" pb_name
+# following the http://docs.ansible.com/ansible/playbooks_best_practices.html#directory-layout to create directory layout
+
+read -p "Set playbook name [ace_playbook]:" pb_name
+[ "$pb_name" == "" ] && pb_name=ace_playbook
 
 read -p "please enter hostname where playbook worked on [localhost]:" host
-[ "$host" == "" ] || host=localhost
+[ "$host" == "" ] && host=localhost
 
-read -p "please enter roles (multi-role separated by comma):" tmp_roles
+read -p "please enter roles (multi-role separated by comma) [git,vim]:" tmp_roles
+[ "$tmp_roles" == "" ] && tmp_roles='git,vim'
 roles="roles/{common,$tmp_roles}"
 
+#------ start to create diretory layout
 eval "mkdir -p $pb_name/$roles/{tasks,vars}"
 eval "touch $pb_name/$roles/{tasks,vars}/main.yml"
 
 # create site.yml 
-cat > $pb_name/site.yml<<CAT
+site=$pb_name/site.yml
+cat > $site<<CAT
 ---
 - name: $pb_name
-  host: $host
+  hosts: $host
 
   roles:
-    - common
 CAT
-for role in $(echo "$tmp_roles"|tr ',' ' ')
+
+roles_dir=$pb_name/roles
+for role in $(cd $roles_dir;echo *)
 do
-    echo "    - $role" >> $pb_name/site.yml
+    echo "    - $role" >> $site
 done
+
+# create production file
+production=$pb_name/production
+[ -f $production ] && cp $production ${production}.bak
+cat > $production<<CAT
+# About the sample, please refer to /etc/ansible/hosts.
+CAT
+
+if [ "$host" == "localhost" ];then
+    echo "localhost ansible_connection=local" >> $production
+else
+    echo $host >> $production
+fi
 
 echo
 echo ">>>>> Playbook Template created as below:"
-ls -R $pb_name | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'
+find $pb_name -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'
 
